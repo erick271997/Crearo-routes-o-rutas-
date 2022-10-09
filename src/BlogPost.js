1c4c2ad105cd0b7fafc1f0c9a941b8fd28c02411
Erick <erickcampaz@gmail.com>
2022-10-09 08:43:07 -0500

    Creado botones para volver donde estbas luego de navegar

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import {  useParams,useNavigate } from 'react-router-dom';
 import  blogdata  from './blogdata'
 
 function BlogPost() {
+    const navigate = useNavigate();
   const { slug } = useParams();
 
   const blogpost = blogdata.find(post => post.slug === slug);
 
+  const returnToBlog = () =>{ //Boton para volver atras
+      navigate('/blog');
+      /* navigate(-1), tambie se puede asi pero este es mejor ya que cuando le das te lleva donde estabas primero */
+  };
+  const returnPrincipal = () =>{
+      navigate(-1); //este lleva si pusiste la url donde vas  y luego pone y luego le das volver donde estabas 
+  }
+
   return (
     <>
       <h1>{blogpost.title}</h1>
+      <button onClick={returnToBlog}>Volver atras</button>
+      <button onClick={returnPrincipal}>Va a la pagina donde estasbas</button>
       <p>{blogpost.author}</p>
       <p>{blogpost.content}</p>
     </>
